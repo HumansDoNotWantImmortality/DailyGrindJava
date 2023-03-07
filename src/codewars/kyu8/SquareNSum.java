1db0f9a97ff6e7afc5abc1f63ddfffaab1537c2d
Krzysztof Rosiński <rosinski.krzysztof.01@gmail.com>
2023-03-07 19:38:03 +0100

    CodeWars/8kyu/Square(n) Sum/Solution

---
+++ src/codewars/kyu8/SquareNSum.java
@@ -0,0 +1,9 @@
+package codewars.kyu8;
+
+import java.util.Arrays;
+
+public class SquareNSum {
+    public static int squareSum(int[] n) {
+        return Arrays.stream(n).map(x -> x * x).sum();
+    }
+}
