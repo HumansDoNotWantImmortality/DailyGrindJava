d0b6c5504cbe6b364873fa3bf759a8497795894c
Krzysztof Rosiński <rosinski.krzysztof.01@gmail.com>
2023-03-08 20:47:17 +0100

    CodeWars/8kyu/Basic Mathematical Operations/Solution

---
+++ src/codewars/kyu8/BasicOperations.java
@@ -0,0 +1,23 @@
+package codewars.kyu8;
+
+public class BasicOperations {
+    public static Integer basicMath(String op, int v1, int v2) {
+        switch (op) {
+            case "-" -> {
+                return v1 - v2;
+            }
+            case "+" -> {
+                return v1 + v2;
+            }
+            case "*" -> {
+                return v1 * v2;
+            }
+            case "/" -> {
+                if (v2 == 0)
+                    throw new IllegalArgumentException("Division by 0");
+                return v1 / v2;
+            }
+            default -> throw new IllegalArgumentException("What even is this?: " + op);
+        }
+    }
+}
