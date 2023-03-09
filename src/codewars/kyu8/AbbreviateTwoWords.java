ad31e3e696d4552b69e9012ba9316d58579c48c4
Krzysztof Rosiński <rosinski.krzysztof.01@gmail.com>
2023-03-09 20:42:28 +0100

    CodeWars/8kyu/Abbreviate a Two Word Name/Solution

---
+++ src/codewars/kyu8/AbbreviateTwoWords.java
@@ -0,0 +1,8 @@
+package codewars.kyu8;
+
+public class AbbreviateTwoWords {
+    public static String abbrevName(String name) {
+        String[] names = name.split(" ");
+        return (names[0].charAt(0) + "." + names[1].charAt(0)).toUpperCase();
+    }
+}
