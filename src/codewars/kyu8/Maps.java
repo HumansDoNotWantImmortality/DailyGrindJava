7337740eeefd53387d261d2ac317a8b37b0ee546
Krzysztof Rosiński <rosinski.krzysztof.01@gmail.com>
2023-03-10 22:29:19 +0100

    CodeWars/8kyu/Beginner - Lost Without a Map/Solution

---
+++ src/codewars/kyu8/Maps.java
@@ -0,0 +1,9 @@
+package codewars.kyu8;
+
+import java.util.Arrays;
+
+public class Maps {
+    public static int[] map(int[] arr) {
+        return Arrays.stream(arr).map(x -> x * 2).toArray();
+    }
+}
