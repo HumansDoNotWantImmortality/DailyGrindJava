443e38cad876162f98bfc3c820760416c4d101b4
Krzysztof Rosiński <rosinski.krzysztof.01@gmail.com>
2023-03-14 21:39:36 +0100

    CodeWars/7kyu/Descending Order/Solution

---
+++ src/codewars/kyu7/GetTheMiddleCharacter.java
@@ -0,0 +1,8 @@
+package codewars.kyu7;
+
+public class GetTheMiddleCharacter {
+    public static String getMiddle(String word) {
+        int length = word.length();
+        return (length % 2 != 0) ?  String.valueOf(word.charAt(length / 2)) : word.substring(length / 2 - 1, length / 2 + 1);
+    }
+}
