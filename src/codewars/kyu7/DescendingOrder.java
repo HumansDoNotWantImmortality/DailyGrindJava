d40f4b4834488eea1f36d613e7bd788f8e576e47
Krzysztof Rosiński <rosinski.krzysztof.01@gmail.com>
2023-03-14 21:38:41 +0100

    CodeWars/7kyu/Descending Order/Solution

---
+++ src/codewars/kyu7/DescendingOrder.java
@@ -0,0 +1,12 @@
+package codewars.kyu7;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        String[] array = String.valueOf(num).split("");
+        Arrays.sort(array, Collections.reverseOrder());
+        return Integer.valueOf(String.join("", array));
+    }
+}
