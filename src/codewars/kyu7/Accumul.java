53ba1969c15a95b026e49adc081c1685157e066b
Krzysztof Rosiński <rosinski.krzysztof.01@gmail.com>
2023-03-15 22:35:32 +0100

    CodeWars/7kyu/Mumbling/Solution

---
+++ src/codewars/kyu7/Accumul.java
@@ -0,0 +1,21 @@
+package codewars.kyu7;
+
+public class Accumul {
+    public static String accum(String s) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+
+        for(char ch : s.toCharArray()) {
+            if(i > 0) {
+                sb.append('-');
+            }
+            sb.append(Character.toUpperCase(ch));
+            for(int j = 0; j < i; j++) {
+                sb.append(Character.toLowerCase(ch));
+            }
+            i++;
+        }
+
+        return sb.toString();
+    }
+}
