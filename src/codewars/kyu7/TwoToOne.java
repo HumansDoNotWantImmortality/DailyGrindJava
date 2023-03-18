9e3a81c12ccd835b1038ecf7897c4a66d3c7e90e
Krzysztof Rosiński <rosinski.krzysztof.01@gmail.com>
2023-03-18 13:05:28 +0100

    CodeWars/7kyu/Two to One/Solution

---
+++ src/codewars/kyu7/TwoToOne.java
@@ -0,0 +1,10 @@
+package codewars.kyu7;
+
+public class TwoToOne {
+
+    public static String longest (String s1, String s2) {
+        StringBuilder sb = new StringBuilder();
+        (s1 + s2).chars().distinct().sorted().forEach(c -> sb.append((char) c));
+        return sb.toString();
+    }
+}
