2abe7b545a7098ba79b5dfe2c7706e8d2c4c103e
Krzysztof Rosiński <rosinski.krzysztof.01@gmail.com>
2023-03-18 13:09:01 +0100

    CodeWars/7kyu/Find the next perfect square!/Solution

---
+++ src/codewars/kyu7/NumberFun.java
@@ -0,0 +1,10 @@
+package codewars.kyu7;
+
+public class NumberFun {
+
+    public static long findNextSquare(long sq) {
+
+        long root = (long) Math.sqrt(sq);
+        return root * root == sq ? (root + 1) * (root + 1) : -1;
+    }
+}
