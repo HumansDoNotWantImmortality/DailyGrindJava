9b4be04ce899514af3da045b8af8caa64a081643
Krzysztof Rosiński <rosinski.krzysztof.01@gmail.com>
2023-04-29 23:25:16 +0200

    feat: problem solution added: - HackerRank/Easy/Java Strings Introduction

---
+++ src/hackerrank/JavaStringsIntroduction.java
@@ -0,0 +1,27 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class JavaStringsIntroduction {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+
+        System.out.println(A.length() + B.length());
+
+        if (A.charAt(0) > B.charAt(0)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        System.out.println(
+                A.substring(0,1).toUpperCase() + A.substring(1)
+                        + " "
+                        + B.substring(0,1).toUpperCase() + B.substring(1)
+        );
+    }
+}
