ef9de37b34c4c7d6fa902b211b5a294fcf933676
Krzysztof Rosiński <rosinski.krzysztof.01@gmail.com>
2023-06-22 11:38:44 +0200

    feat: CodeWars/6kyu/Delete occurrences of an element if it occurs more than n times solution added

---
+++ src/codewars/kyu6/EnoughIsEnough.java
@@ -0,0 +1,15 @@
+package codewars.kyu6;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class EnoughIsEnough {
+
+    public static int[] deleteNth(int[] elements, int maxOccurrences) {
+        Map<Integer, Integer> occurrence = new HashMap<>();
+        return IntStream.of(elements)
+                .filter(motif -> occurrence.merge(motif, 1, Integer::sum) <= maxOccurrences)
+                .toArray();
+    }
+}
